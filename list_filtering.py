e3fd9bc9ea0202fc990d6ab66899bfde24db3849
MilanN <n.milan89@gmail.com>
2023-11-14 22:06:44 +0600

    add new file: list_filtering.py

---
+++ list_filtering.py
@@ -0,0 +1,13 @@
+# In this kata you will create a function that takes a list of non-negative
+# integers and strings and returns a new list with the strings filtered out.
+
+def filter_list(l: list) -> list:
+    return list(filter(lambda n: isinstance(n, int), l))
+
+
+
+if __name__ == '__main__':
+    print(filter_list([1, 2, 'a', 'b']))
+    assert filter_list([1, 2, 'a', 'b']) == [1, 2]
+    assert filter_list([1, 'a', 'b', 0, 15]) == [1, 0, 15]
+    assert filter_list([1, 2, 'aasf', '1', '123', 123]) == [1, 2, 123]
