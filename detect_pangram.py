cada758f89bd8d5b487c35d8003c511b075d00be
MilanN <n.milan89@gmail.com>
2024-01-08 20:10:30 +0600

    refactorng: detect_pangram.py, > import, as

--- detect_pangram.py
+++ detect_pangram.py
@@ -1,19 +1,19 @@
-import string
+from string import ascii_lowercase as AL
 
 
 # def is_pangram(s: str) -> bool:
-#     a = set(x for x in string.ascii_lowercase)
+#     a = set(x for x in AL)
 #     b = set(x for x in s.lower() if x in a)
 #
 #     return True if b.intersection(a) == a else False
 
 
-def is_pangram(s):
-    return set(string.ascii_lowercase).issubset(s.lower())
+def is_pangram(s: str) -> bool:
+    return set(AL).issubset(s.lower())
 
 
 if __name__ == '__main__':
     print(is_pangram("The quick, brown fox jumps over the lazy dog!"))
-    c = 1
+
     assert is_pangram("The quick, brown fox jumps over the lazy dog!") == True
     assert is_pangram("1bcdefghijklmnopqrstuvwxyz") == False
