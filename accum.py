6efc164e7b11132c800e2c8b2f97085be7093b98
Milan Nasrytdinov <milan.nasrytdinov@gmail.com>
2024-04-26 15:47:02 +0600

    new files: accum.py, array_diff.py, dna_to_rna.py

---
+++ accum.py
@@ -0,0 +1,12 @@
+def accum(st: str) -> str:
+    return '-'.join((s * i).title() for i, s in enumerate(st, 1))
+
+
+if __name__ == '__main__':
+    print(accum("NyffsGeyylB"))
+
+    assert accum("ZpglnRxqenU") == "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
+    assert accum("NyffsGeyylB") == "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb"
+    assert accum("MjtkuBovqrU") == "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu"
+    assert accum("EvidjUnokmM") == "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm"
+    assert accum("HbideVbxncC") == "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc"
